f215cf038770188d22a64a679a8bd69f932c8ce2
Kalvin <kalvin.kao@hotmail.com>
2020-10-19 09:06:26 -0400

    change scenes

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -6,10 +6,13 @@ import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -30,11 +33,29 @@ public class GameController {
 	int [] diamonds = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 
 
+
 	@FXML
 	public void playButton() {
+
 	   // playButton.getScene().setRoot(game.getRootPane());
+            stage = (Stage) playButton.getScene().getWindow();
+
+			try {
+				VBox root = (VBox) FXMLLoader.load(getClass().getResource("cards.fxml"));
+				Scene scene = new Scene(root, 1280, 800);
+	            stage.setScene(scene);
+	            stage.show();
+				scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+				stage.setScene(scene);
+				stage.setResizable(false);
+				stage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 	}
+
 
 
 	@FXML
--- src/application/Main.java
+++ src/application/Main.java
@@ -35,9 +35,5 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-
-
-
-
 	}
 }
